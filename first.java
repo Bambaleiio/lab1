aaff6cd1820d4617d4fa3be2734dd49488b188da
Arseny <Bambaleiio@yandex.ru>
2022-11-24 18:34:43 +0300

    add first.java

---
+++ first.java
@@ -0,0 +1,27 @@
+package lw1;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+public class first {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int day, month, year;
+        System.out.print("Day of birth: ");
+        day = scanner.nextInt();
+        System.out.print("Month of birth: ");
+        month = scanner.nextInt();
+        System.out.print("Year of birth: ");
+        year = scanner.nextInt();
+
+        LocalDate dateOfBirth = LocalDate.of(year, month, day);
+        LocalDate now = LocalDate.now();
+
+        Period diff = Period.between(dateOfBirth, now);
+
+        System.out.printf("%d years, %d months and %d days",
+                diff.getYears(), diff.getMonths(), diff.getDays());
+    }
+}
